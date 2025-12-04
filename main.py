b3789d45ff70fce935f375c18953a732e2f313df
Ebay <eka.bayu@bignet.id>
2025-12-04 17:06:59 +0700

    day 1 belajar python

---
+++ main.py
@@ -0,0 +1,27 @@
+print("Selamat pagi dunia tipu-tipu!!!")
+
+string=str
+nama="U can Call me GABAY/Ebay"
+
+integer=int
+angka=2007
+
+boolean=bool
+bernilai_benar=True
+bernilai_salah=False
+
+floating=float
+nilai=98.2
+
+lists=list
+buah=["apel", "jeruk", "anggur"]
+
+tuples=tuple
+namaIT=("Budi", "Faza", "Lendra")
+
+print(nama)
+print(namaIT)
+print(nilai)
+print(buah)
+print(bernilai_benar)
+print(bernilai_salah)
