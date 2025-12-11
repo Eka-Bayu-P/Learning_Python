eec7e07987440f167ae4aba871bff65ee42d1f8b
Ebay <eka.bayu@bignet.id>
2025-12-11 16:26:28 +0700

    day 2 belajar variable python

---
+++ variable.py
@@ -0,0 +1,11 @@
+variable_name="Lendskuy"
+angka_eka= 20
+angka_paza=10
+
+angka_batagor=angka_eka
+angka_paza=variable_name
+
+print(variable_name)
+print(angka_eka)
+print(angka_paza)
+print(angka_batagor)
